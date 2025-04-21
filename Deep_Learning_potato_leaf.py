795a451dac9956dc43aff3eb8b3b81cba68391a3
bsprakashbs <167135326+bsprakashbs@users.noreply.github.com>
2025-04-21 15:38:25 +0100

    Commit Prediction
    Signed-off-by: bsprakashbs <167135326+bsprakashbs@users.noreply.github.com>

--- Deep_Learning_potato_leaf.py
+++ Deep_Learning_potato_leaf.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 from tensorflow.keras import models,layers
 from tensorflow.keras.layers import Resizing, RandomFlip, RandomRotation ,Rescaling
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 #-----------------------------------------Load Image using TEnsor flow ----------------------------------------#
 #256 ----> 0 to 255 RGB scale
+TF_ENABLE_ONEDNN_OPTS=0
 IMAGE_SIZE=256
 BATCH_SIZE=32
 CHANNELS=3
@@ -63,7 +65,7 @@ resize_and_rescale = tf.keras.Sequential([
     Resizing(IMAGE_SIZE,IMAGE_SIZE),
     Rescaling(1.0/255)
 ])
-#----------------------------------------Data Augmentation  -- pick same iamge train that in to  contrast,horizontal flip,rotation,zoom etc....----------------#
+#----------------------------------------Data Augmentation  -- pick same image train that in to  contrast,horizontal flip,rotation,zoom etc....----------------#
 data_augmentation = tf.keras.Sequential([
     RandomFlip("horizontal_and_vertical"),
     RandomRotation(0.2)
@@ -105,12 +107,14 @@ model.compile(
 ######  Checking how model accuracy is getting improved by giving epochs=50  - 50 times it re fit the model by passing different data set and provides the accuracy
 ###### cpu - will consume lot
 ###### GPU -  it takes less time.
+
+EPOCHS = 20
 history = model.fit(
                     train_ds,
                     batch_size=BATCH_SIZE,
                     validation_data=val_ds,
                     verbose=1,
-                    epochs=2,
+                    epochs=EPOCHS,
                     )
 
 print(history)
@@ -123,22 +127,82 @@ scores = model.evaluate(test_ds)
 print(scores)
 
 #---------------------------------------- plotting  to see how accuracy is increased..----------------#
-EPOCHS = 20
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+print('-----print(acc)-----------')
+print(np.array(history.history['accuracy']))
+print('-----print(val_acc)-----------')
+print(np.array(history.history['val_accuracy']))
+#
+acc = np.array(history.history['accuracy'])
+print('-----print(acc)-----------')
+print(acc)
+#
+val_acc = np.array(history.history['val_accuracy'])
+print('---------print(val_acc)--------')
+print(val_acc)
+#
+print('---------length of plotting acc and val_acc values--------')
+print(f"acc: {acc}, len: {len(acc)}")
+print(f"val_acc: {val_acc}, len: {len(val_acc)}")
+loss = np.array(history.history['loss'])
+val_loss = np.array(history.history['val_loss'])
+plt.grid(True)
+#plt.xlim(0, 1)
+#plt.ylim(0, 1)
 plt.figure(figsize=(8, 8))
+print('---------worked 1--------')
 plt.subplot(1, 2, 1)
+print('---------worked 1.1--------')
 plt.plot(range(EPOCHS), acc, label='Training Accuracy')
+#plt.plot(range(len(acc)), acc, label='Training Accuracy')
 plt.plot(range(EPOCHS), val_acc, label='Validation Accuracy')
+#plt.plot(range(len(val_acc)), val_acc, label='Validation Accuracy')
+
+print('---------worked 1.2--------')
 plt.legend(loc='lower right')
 plt.title('Training and Validation Accuracy')
-
+print('---------worked 2--------')
 plt.subplot(1, 2, 2)
+#plt.plot(range(len(loss)), loss, label='Training Loss')
 plt.plot(range(EPOCHS), loss, label='Training Loss')
+#plt.plot(range(len(val_loss)), val_loss, label='Validation Loss')
 plt.plot(range(EPOCHS), val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
+
+
+#---------------------------------------- Run prediction on a sample single image..----------------#
+'''
+for images_batch, labels_batch in test_ds.take(1):
+    first_image = images_batch[0].numpy().astype('uint8')
+    first_label = labels_batch[0].numpy()
+
+    print("first image to predict")
+    plt.imshow(first_image)
+    print("actual label:", class_names[first_label])
+
+    batch_prediction = model.predict(images_batch)
+    print("predicted label:", class_names[np.argmax(batch_prediction[0])])
+'''
+
+# ---------------------------------------- Run prediction on a multipel images ..----------------#
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0)
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+
+    plt.figure(figsize=(15, 15))
+    for images, labels in test_ds.take(1):
+        for i in range(9):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            predicted_class, confidence = predict(model, images[i].numpy())
+            actual_class = class_names[labels[i]]
+            plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+            plt.axis("off")
+            plt.show()
